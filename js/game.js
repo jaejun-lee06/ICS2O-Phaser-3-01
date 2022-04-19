ae6e96fb11cdf232580bc5f7cd54c9a8388451f6
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-04-19 16:58:36 +0000

    added comments

--- js/game.js
+++ js/game.js
@@ -1,10 +1,29 @@
 /* global Phaser */
 
+// Copyright (c) 2022 Jaejun Lee All rights reserved
+//
+// Created by: Jaejun Lee
+// Created on: April 2022
+// This is the Phaser3 configuration file
+
+//* Game scene*/
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  //set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phager.Scale.FIT,
+    //we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
