abc467ba9ce5469aeba149529b918d00cb1a6f30
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-04-19 02:49:30 +0000

    done

---
+++ js/game.js
@@ -0,0 +1,14 @@
+/* global Phaser */
+const config = {
+type: Phaser.AUTO,
+width: 1920,
+height: 1080,
+
+ICS2O
+
+Programs must be written for people to read, and only incidentally for machines to execute. - H. Abelson and G. Sussmanyou
+
+backgroundColor: 0x5f6e7a
+}
+const game = new Phaser.Game(config)
+console.log(game)
