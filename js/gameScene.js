0df5e43731464309a68b6e0fb7d92057494ca4ed
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-05-30 17:22:20 +0000

    0309

--- js/gameScene.js
+++ js/gameScene.js
@@ -49,7 +49,7 @@ class GameScene extends Phaser.Scene {
     }
     if (keyRightObj.isDown === true) {
       this.ship.x += 15
-      if (this.ship.x < 1920) {
+      if (this.ship.x > 1920) {
         this.ship.x = 1920
       }
     }
